ee11fc0bb81c3baa22a01e2f122f68c61a09570d
Andrew Nesbitt <andrewnez@gmail.com>
2025-07-10 10:35:17 +0100

    Add User-Agent header to fetch request for package ping

--- ping.js
+++ ping.js
@@ -19,7 +19,9 @@
       if (name) {
         console.log(name)
         const cacheBuster = Math.floor(Math.random() * 1000000)
-        fetch('https://packages.ecosyste.ms/api/v1/registries/npmjs.org/packages/' + name + '/ping?cb=' + cacheBuster)
+        fetch('https://packages.ecosyste.ms/api/v1/registries/npmjs.org/packages/' + name + '/ping?cb=' + cacheBuster, {
+          headers: { 'User-Agent': 'npm-update-stream' }
+        })
           .then(res => console.log(`statusCode: ${res.status}`))
       }
     }
